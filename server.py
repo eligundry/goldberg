0b961bdec9a3d8f02e056ea7401dd54d3af162bf
Eli Gundry <eligundry@gmail.com>
2014-10-25 00:11:06 -0400

    Created index route to give list of tasks

--- server.py
+++ server.py
@@ -2,5 +2,27 @@ from flask.ext.api import FlaskAPI
 
 app = FlaskAPI(__name__)
 
+tasks = {
+    "lights": {
+        "name": "Lights",
+        "description": "Control the lights"
+    },
+    "water": {
+        "name": "Water",
+        "description": "Water your plant"
+    },
+    "cat": {
+        "name": "Cat",
+        "description": "Feed your cat"
+    }
+}
+
+@app.route("/", methods=['GET'])
+def index():
+    """
+    Returns a list of actions to the Pebble
+    """
+    return tasks
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
